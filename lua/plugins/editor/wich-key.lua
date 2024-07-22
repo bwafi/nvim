ba396edea7c073eea7d1e8945aa6900257fff105
Syahroni Bawafi <syahronibawafi@gmail.com>
2024-07-22 13:05:48 +0700

    fix update plugin

--- lua/plugins/editor/wich-key.lua
+++ lua/plugins/editor/wich-key.lua
@@ -3,17 +3,17 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     config = function()
-      require("which-key").register({
-        ["<leader>c"] = { name = "Code", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "Debug", _ = "which_key_ignore" },
-        ["<leader>g"] = { name = "Git", _ = "which_key_ignore" },
-        ["<leader>h"] = { name = "More git", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "Rename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "Search", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "Workspace", _ = "which_key_ignore" },
-        ["<leader>t"] = { name = "Test", _ = "which_key_ignore" },
-        ["<leader>u"] = { name = "Toggle", _ = "which_key_ignore" },
-        ["<leader>x"] = { name = "Diagnostics/quickfix", _ = "which_key_ignore" },
+      require("which-key").add({
+        { "<leader>c", group = "Code" },
+        { "<leader>d", group = "Debug" },
+        { "<leader>g", group = "Git" },
+        { "<leader>h", group = "More git" },
+        { "<leader>r", group = "Rename" },
+        { "<leader>s", group = "Search" },
+        { "<leader>t", group = "Test" },
+        { "<leader>u", group = "Toggle" },
+        { "<leader>w", group = "Workspace" },
+        { "<leader>x", group = "Diagnostics/quickfix" },
       })
     end,
   },
