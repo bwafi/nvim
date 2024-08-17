7a5ee8a8a9ca361b1f7048ffc912063b8a2c1622
Syahroni Bawafi <syahronibawafi@gmail.com>
2024-08-17 22:38:14 +0700

    feat: Change comment nvim to comment builtin neovim and ts-comments.nvim

--- lua/plugins/coding/comment.lua
+++ lua/plugins/coding/comment.lua
@@ -1,22 +1,31 @@
 return {
+  -- {
+  --   "echasnovski/mini.comment",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     options = {
+  --       custom_commentstring = function()
+  --         return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+  --       end,
+  --     },
+  --   },
+  -- },
+  --
+  -- {
+  --   "JoosepAlviste/nvim-ts-context-commentstring",
+  --   event = "VeryLazy",
+  --   -- lazy = true,
+  --   opts = {
+  --     enable_autocmd = false,
+  --   },
+  --
+  -- },
+  --
+  --
+  -- comments
   {
-    "echasnovski/mini.comment",
+    "folke/ts-comments.nvim",
     event = "VeryLazy",
-    opts = {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-        end,
-      },
-    },
-  },
-
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    event = "VeryLazy",
-    -- lazy = true,
-    opts = {
-      enable_autocmd = false,
-    },
+    opts = {},
   },
 }
