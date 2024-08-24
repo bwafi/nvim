e9c748b0044029b5bbe1123d2cd6add8b6d53e66
Syahroni Bawafi <syahronibawafi@gmail.com>
2024-08-24 14:37:28 +0700

    refactor: Change autopairs to mini-pairs nvim

--- lua/plugins/editor/mini-nvim.lua
+++ lua/plugins/editor/mini-nvim.lua
@@ -96,4 +96,16 @@ return {
       end
     end,
   },
+
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      skip_ts = { "string" },
+      skip_unbalanced = true,
+      markdown = true,
+    },
+  },
 }
