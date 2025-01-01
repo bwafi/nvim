22a6c70c72ad74b513a2c094f879909c5add8ebc
bwafi <syahronibwf@gmail.com>
2025-01-01 07:12:18 +0700

    chore: Update

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -43,8 +43,6 @@ return {
     -- sync_install = true,
     highlight = {
       enable = true,
-      additional_vim_regex_highlighting = false,
-      -- additional_vim_regex_highlighting = { "ruby" },
     },
     indent = { enable = true, disable = { "ruby" } },
 
