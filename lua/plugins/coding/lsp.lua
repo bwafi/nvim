d9d9ed8afc188d81984bef8221652763d694372a
Syahroni Bawafi <syahronibwf@gmail.com>
2025-01-24 08:59:45 +0700

    chore: add setting for vtsls server

--- lua/plugins/coding/lsp.lua
+++ lua/plugins/coding/lsp.lua
@@ -1,3 +1,5 @@
+local utils = require("utils.lsp")
+
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
@@ -195,6 +197,132 @@ return {
               semicolon = "Disable",
               arrayIndex = "Disable",
             },
+          },
+        },
+      },
+
+      vtsls = {
+        -- explicitly add default filetypes, so that we can extend
+        -- them in related extras
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        settings = {
+          complete_function_calls = true,
+          vtsls = {
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              maxInlayHintLength = 30,
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
+            },
+          },
+        },
+        keys = {
+          {
+            "gD",
+            function()
+              local params = vim.lsp.util.make_position_params()
+              utils.execute({
+                command = "typescript.goToSourceDefinition",
+                arguments = { params.textDocument.uri, params.position },
+                open = true,
+              })
+            end,
+            desc = "Goto Source Definition",
+          },
+          {
+            "gR",
+            function()
+              utils.execute({
+                command = "typescript.findAllFileReferences",
+                arguments = { vim.uri_from_bufnr(0) },
+                open = true,
+              })
+            end,
+            desc = "File References",
+          },
+          {
+            "<leader>co",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.organizeImports" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Organize Imports",
+          },
+          {
+            "<leader>cM",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.addMissingImports.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Add missing imports",
+          },
+          {
+            "<leader>cu",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.removeUnused.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Remove unused imports",
+          },
+          {
+            "<leader>cD",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.fixAll.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Fix all diagnostics",
+          },
+          {
+            "<leader>cV",
+            function()
+              utils.execute({
+                command = "typescript.selectTypeScriptVersion",
+              })
+            end,
+            desc = "Select TS workspace version",
           },
         },
       },
