53c24df1b06cdec4494b8bd842d6bab1a340eb88
Syahroni Bawafi <syahronibwf@gmail.com>
2025-01-30 12:17:02 +0700

    feat: replace `mini.pairs` with `nvim-autopairs`

--- lua/plugins/editor/mini-nvim.lua
+++ lua/plugins/editor/mini-nvim.lua
@@ -22,18 +22,6 @@ return {
   },
 
   {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    opts = {
-      modes = { insert = true, command = true, terminal = false },
-      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-      skip_ts = { "string" },
-      skip_unbalanced = true,
-      markdown = true,
-    },
-  },
-
-  {
     "echasnovski/mini.files",
     opts = {
       windows = {
